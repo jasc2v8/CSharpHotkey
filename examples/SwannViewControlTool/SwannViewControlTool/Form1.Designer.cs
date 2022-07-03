1798f11bb3388dff37f101c9c25456831c3a2644
jasc2v8 <jasc2v8@yahoo.com>
2022-07-02 20:20:00 -0700

    v0.0.10

---
+++ examples/SwannViewControlTool/SwannViewControlTool/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SwannViewControlTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ContextMenuItemOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.ContextMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "SwannView Control Tool";
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ContextMenuItemOpen,
+            this.ContextMenuItemExit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            this.contextMenuStrip1.Text = "Context Menu";
+            // 
+            // ContextMenuItemOpen
+            // 
+            this.ContextMenuItemOpen.Name = "ContextMenuItemOpen";
+            this.ContextMenuItemOpen.Size = new System.Drawing.Size(180, 22);
+            this.ContextMenuItemOpen.Text = "SwannView Link";
+            this.ContextMenuItemOpen.Click += new System.EventHandler(this.ContextMenuItemOpen_Click);
+            // 
+            // ContextMenuItemExit
+            // 
+            this.ContextMenuItemExit.Name = "ContextMenuItemExit";
+            this.ContextMenuItemExit.Size = new System.Drawing.Size(180, 22);
+            this.ContextMenuItemExit.Text = "Exit";
+            this.ContextMenuItemExit.Click += new System.EventHandler(this.ContextMenuItemExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 206);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ContextMenuItemOpen;
+        private System.Windows.Forms.ToolStripMenuItem ContextMenuItemExit;
+    }
+}
+
---
+++ examples/SwannViewControlTool/SwannViewControlTool/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+using CSharpHotkeyLib;
+
+namespace SwannViewControlTool
+{
+    public partial class Form1 : Form
+    {
+
+        private const string LOGIN_WINDOW_TITLE = "Search & Login";
+        private const string MYDVR_WINDOW_TITLE ="SwannView Link";
+        private const string EXE_NAME = @"C:\Program Files (x86)\SwannView Link\MyDVR.exe";
+
+        private CSharpHotkey Win = new CSharpHotkey();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Hide();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = Assembly.GetEntryAssembly().GetName().Name + " v" + Assembly.GetEntryAssembly().GetName().Version;
+            notifyIcon1.Visible = true;
+            Win.SetTitleMatchMode(MatchMode.Contains, MatchCase.InSensitive);
+
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+        private void ContextMenuItemExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void ContextMenuItemOpen_Click(object sender, EventArgs e)
+        {
+            DoStart();
+        }
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //handle left click only. right click is handled by the context menu
+            if (e.Button == MouseButtons.Left)
+            {
+                DoStart();
+            }
+        }
+        private void DoStart()
+        {
+            Process.Start(EXE_NAME);
+
+            if (Win.Wait(LOGIN_WINDOW_TITLE, 15))
+            {
+                this.Show();
+                MessageBox.Show("Timeout waiting for: " + LOGIN_WINDOW_TITLE);
+                return;
+            }
+
+            //The Search & Login window is open and the default button is Login
+            //press the Login button
+            Win.Send(Keys.Enter);
+
+            if (Win.Wait(MYDVR_WINDOW_TITLE, 15))
+            {
+                this.Show();
+                MessageBox.Show("Timeout waiting for: " + MYDVR_WINDOW_TITLE);
+                return;
+            }
+
+            Win.Sleep(250);
+
+            //the DVR window is open and maximized
+            //press the window split button (my display is 1920x1080)
+            Win.Click(MouseButton.Left, 1598,1060);
+
+            Win.Sleep(250);
+
+            //press the 4 window button (my display is 1920x1080)
+            Win.Click(MouseButton.Left, 1660, 995);
+
+            Win.WaitClose(MYDVR_WINDOW_TITLE);
+
+            if (Win.Wait(LOGIN_WINDOW_TITLE, 15))
+            {
+                //If timeout, user pressed X to close window so do nothing
+                return;
+            }
+
+            //if no timeout, user pressed logout so close the login window
+            Win.Sleep(250);
+            Win.Click(MouseButton.Left, 1280, 740);
+        }
+    }
+}
